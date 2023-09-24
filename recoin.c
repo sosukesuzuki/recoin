aeda710504093b6e58dc9557efa75da47173a60b
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-09-25 02:36:25 +0900

    Update comments and support EOF

--- recoin.c
+++ recoin.c
@@ -10,18 +10,18 @@ enum TokenSyms {
   TK_EOT,
   TK_CHAR,
   TK_STRING,
-  TK_VERTICAL_LINE,        // U+007C
-  TK_REVERSE_SOLIDUS,      // U+005C
-  TK_LEFT_PARENTHESIS,     // U+0028
-  TK_RIGHT_PARENTHESIS,    // U+0029
-  TK_LEFT_SQUARE_BRACKET,  // U+005B
-  TK_RIGHT_SQUARE_BRACKET, // U+005D
-  TK_QUESTION_MARK,        // U+003F
-  TK_PLUS_SIGN,            // U+002B
-  TK_ASTERISK,             // U+002A
-  TK_DOLLAR_SIGN,          // U+0024
-  TK_CIRCUMFLEX_ACCENT,    // U+005E
-  TK_HYPHEN_MINUS          // U+002D
+  TK_VERTICAL_LINE,        // U+007C |
+  TK_REVERSE_SOLIDUS,      // U+005C (\)
+  TK_LEFT_PARENTHESIS,     // U+0028 (
+  TK_RIGHT_PARENTHESIS,    // U+0029 )
+  TK_LEFT_SQUARE_BRACKET,  // U+005B [
+  TK_RIGHT_SQUARE_BRACKET, // U+005D ]
+  TK_QUESTION_MARK,        // U+003F ?
+  TK_PLUS_SIGN,            // U+002B +
+  TK_ASTERISK,             // U+002A *
+  TK_DOLLAR_SIGN,          // U+0024 $
+  TK_CIRCUMFLEX_ACCENT,    // U+005E ^
+  TK_HYPHEN_MINUS          // U+002D -
 };
 
 typedef struct {
@@ -57,6 +57,10 @@ int fetch_token(RecoinToken *token, UChar **src) {
   } else if (*p == '*') {
     token->type = TK_ASTERISK;
     *src = p + 1;
+    return 1;
+  } else if (*p == '\0') {
+    token->type = TK_EOT;
+    *src = p;
     return 1;
   }
 
