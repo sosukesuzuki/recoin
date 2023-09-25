5cdae7eed5c387a0d03166fb7ec123adaf410dbd
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2023-09-25 13:10:41 +0900

    Add test cases

--- test_recoin.c
+++ test_recoin.c
@@ -1,16 +1,63 @@
 #include "recoin.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TEST_ASSERT(expr)                                                      \
+  do {                                                                         \
+    if (!(expr)) {                                                             \
+      fprintf(stderr, "Assertion failed: %s (%s:%d)\n", #expr, __FILE__,       \
+              __LINE__);                                                       \
+      exit(1);                                                                 \
+    }                                                                          \
+  } while (0)
+
+#define RUN_TEST(test_func)                                                    \
+  do {                                                                         \
+    printf("Running test: %s\n", #test_func);                                  \
+    test_func();                                                               \
+    printf("Test passed.\n\n");                                                \
+  } while (0)
+
+void fetch_token_str_1() {
+  UChar *src = "abc";
+  RecoinToken token;
+  fetch_token(&token, &src);
+  TEST_ASSERT(token.type == TK_STRING);
+  TEST_ASSERT(strcmp(token.u.s, "abc") == 0);
+}
+void fetch_token_cons_alt_1() {
+  UChar *src = "|";
+  RecoinToken token;
+  fetch_token(&token, &src);
+  TEST_ASSERT(token.type == TK_VERTICAL_LINE);
+}
+void fetch_token_qtfr_asterrisk_1() {
+  UChar *src = "*";
+  RecoinToken token;
+  fetch_token(&token, &src);
+  TEST_ASSERT(token.type == TK_ASTERISK);
+}
+void fetch_token_basic_regexp_1() {
+  UChar *src = "abc|def*";
+  RecoinToken token;
+  fetch_token(&token, &src);
+  TEST_ASSERT(token.type == TK_STRING);
+  TEST_ASSERT(strcmp(token.u.s, "abc") == 0);
+  fetch_token(&token, &src);
+  TEST_ASSERT(token.type == TK_VERTICAL_LINE);
+  fetch_token(&token, &src);
+  TEST_ASSERT(token.type == TK_STRING);
+  TEST_ASSERT(strcmp(token.u.s, "def") == 0);
+  fetch_token(&token, &src);
+  TEST_ASSERT(token.type == TK_ASTERISK);
+}
 
 int main() {
-  UChar *src = "abc|*";
-  RecoinToken token;
-
-  fetch_token(&token, &src);
-  printf("%u\n", token.type);
-  fetch_token(&token, &src);
-  printf("%u\n", token.type);
-  fetch_token(&token, &src);
-  printf("%u\n", token.type);
+  RUN_TEST(fetch_token_str_1);
+  RUN_TEST(fetch_token_cons_alt_1);
+  RUN_TEST(fetch_token_qtfr_asterrisk_1);
+  RUN_TEST(fetch_token_basic_regexp_1);
 
   return 0;
 }
